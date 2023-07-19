34783dd24c2454ab3a04b8b3cd51bcd118b30018
aleksandrbelous <alexandrbelous2003@gmail.com>
2023-07-19 17:45:40 +0400

    feat: check for changes

--- apps/host-1/src/app/app.component.ts
+++ apps/host-1/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'host-1';
   constructor() {
-    // eslint-disable-next-line no-console
+
     console.log(this.title)
     
   }
